844360e39e663a6c9652fe5f90db3f7f5fd39649
kurniawan2805 <42286406+kurniawan2805@users.noreply.github.com>
2022-10-04 22:40:27 +0700

    learn to edit row 1 file modified

--- pages/datagrid.jsx
+++ pages/datagrid.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { supabase } from '../utils/supabaseClient';
 import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+
 // import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 // import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 // import { DatePicker } from '@mui/x-date-pickers/DatePicker';
@@ -50,6 +52,7 @@ const columns = [
     width: 70,
     headerAlign: 'center',
     align: 'center',
+    editable: true,
   },
   {
     field: 'unit_no',
@@ -57,6 +60,7 @@ const columns = [
     width: 120,
     headerAlign: 'center',
     align: 'center',
+    editable: true,
   },
   {
     field: 'survey_type',
@@ -64,6 +68,7 @@ const columns = [
     width: 70,
     headerAlign: 'center',
     align: 'center',
+    editable: true,
   },
   {
     field: 'depot',
@@ -71,6 +76,83 @@ const columns = [
     width: 70,
     headerAlign: 'center',
     align: 'center',
+    editable: true,
+  },
+  {
+    field: 'survey_date',
+    headerName: 'Survey Date',
+    width: 100,
+    editable: true,
+    type: 'date',
+    headerAlign: 'center',
+    align: 'center',
+    // valueFormatter: (params) => dayjs(params?.value).format('DD/MM/YYYY'),
+  },
+  {
+    field: 'submit_date',
+    headerName: 'Submit Date',
+    width: 100,
+    editable: true,
+    type: 'date',
+    headerAlign: 'center',
+    align: 'center',
+    // valueFormatter: (params) => dayjs(params?.value).format('DD/MM/YYYY'),
+  },
+  {
+    field: 'remarks',
+    headerName: 'Remarks',
+    width: 100,
+    editable: true,
+    type: 'text',
+    headerAlign: 'center',
+    align: 'center',
+    // valueFormatter: (params) => dayjs(params?.value).format('DD/MM/YYYY'),
+  },
+  {
+    field: 'action',
+    headerName: 'Action',
+    sortable: false,
+    renderCell: (params) => {
+      const onClick = (e) => {
+        e.stopPropagation(); // don't select this row after clicking
+        console.log(params);
+        // const {
+        //   id,
+        //   customer,
+        //   depot,
+        //   unit_no,
+        //   survey_date,
+        //   submit_date,
+        //   approval_date,
+        // } = params.row;
+
+        const api = params.api;
+        const thisRow = {};
+
+        const updateData = async (attrib) => {
+          const { data, error } = await supabase
+            .from('offhire_db')
+            .update({ depot: depot })
+            .eq('id', id);
+        };
+        api
+          .getAllColumns()
+          .filter((c) => c.field !== '__check__' && !!c)
+          .forEach(
+            (c) => (thisRow[c.field] = params.getValue(params.id, c.field))
+          );
+
+        // console.log(thisRow);
+
+        // return alert(JSON.stringify(thisRow, null, 4));
+      };
+
+      return (
+        <Button variant="outlined" size="small" onClick={onClick}>
+          Save
+        </Button>
+      );
+    },
   },
 ];
 
@@ -80,12 +162,14 @@ export default function ShowTableGrid({ offhire_db }) {
   // console.log(offhire_db);
   const [value, setValue] = useState(null);
   return (
-    <div>
+    <div style={{ width: '100%' }}>
       <DataGrid
         rows={offhire_db}
         columns={columns}
         experimentalFeatures={{ newEditingApi: true }}
-        sx={{ height: '100vh' }}
+        rowHeight={38}
+        sx={{ height: '100vh', width: '70vw', margin: 'auto' }}
+        onCellClick={(params, event) => event.stopPropagation()}
       />
     </div>
   );
