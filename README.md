206b5fb0624d007618765dc94af2843e1138f2da
kurniawan2805 <42286406+kurniawan2805@users.noreply.github.com>
2022-10-04 11:28:40 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dashboard_mui
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-kyayem)
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <h1>Hello</h1>
+    </div>
+  );
+}
